dff0c3ca5532bf9b2df9413317c39875dab24022
Erick Lin <erick_lin@ao.uscourts.gov>
2017-06-08 16:34:37 -0400

    Added auth0 for users authentication service.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs/Observable";
+import auth0 from 'auth0-js';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
@@ -8,6 +9,15 @@ import 'rxjs/add/operator/delay';
 @Injectable()
 export class AuthService {
   isLoggedIn: boolean = false;
+
+  auth0 = new auth0.WebAuth({
+    clientID: '_Yar3zsMuyes5htv43b6MLqThj9tukug',
+    domain: 'angelix.auth0.com',
+    responseType: 'token id_token',
+    audience: 'https://angelix.auth0.com/userinfo',
+    redirectUri: 'http://localhost:14200/callback',
+    scope: 'openid'
+  });
 
   // store the URL so we can redirect after logging in
   redirectUrl: string;
